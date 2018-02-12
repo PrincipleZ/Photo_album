e02d2c0a72c9476dd6e49570cb27170ebe4186f6
Yuanze Zhang <jason950314@gmail.com>
2018-02-12 02:08:42 -0500

    finished photo upload

--- sql_commands.py
+++ sql_commands.py
@@ -34,3 +34,34 @@ def create_album(albumName, description, user_id):
 def get_albums(user_id):
     return "SELECT album_id, name FROM Album WHERE user_id = {}".format(
         user_id)
+
+
+def get_album_owner(album_id):
+    return "SELECT user_id FROM Album WHERE album_id = {}".format(album_id)
+
+
+def create_photo(album_id, name, file_path):
+    return "INSERT INTO Photo (name, album_id, file_path) VALUES ('{}', {}, '{}')".format(
+        name, album_id, file_path)
+
+
+def get_next_photo_id():
+    return "SELECT photo_id FROM Photo ORDER BY photo_id DESC LIMIT 1"
+
+
+def check_photo_id(photo_id):
+    return "SELECT name FROM Photo WHERE photo_id={}".format(photo_id)
+
+
+def get_photo_id(path):
+    return "SELECT photo_id FROM Photo WHERE file_path = '{}'".format(path)
+
+
+def update_file_path(new_path, photo_id):
+    return "UPDATE Photo SET file_path='{}' WHERE photo_id = {}".format(
+        new_path, photo_id)
+
+
+def get_photos_from_album(album_id):
+    return "SELECT name, file_path FROM Photo WHERE album_id = {}".format(
+        album_id)
